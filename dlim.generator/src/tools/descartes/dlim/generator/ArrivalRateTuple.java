275c85c597ca2eb7aad119f25fd03bbf3f41a7a1
joakimkistowski <joakim.kistowski@uni-wuerzburg.de>
2014-11-12 13:14:36 +0100

    Xtext nature removed

--- dlim.generator/src/tools/descartes/dlim/generator/ArrivalRateTuple.java
+++ dlim.generator/src/tools/descartes/dlim/generator/ArrivalRateTuple.java
@@ -35,6 +35,36 @@ public class ArrivalRateTuple implements Comparable<ArrivalRateTuple> {
 		return Math.abs(t.getTimeStamp()-timeStamp);
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(arrivalRate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(timeStamp);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArrivalRateTuple other = (ArrivalRateTuple) obj;
+		if (Double.doubleToLongBits(arrivalRate) != Double
+				.doubleToLongBits(other.arrivalRate))
+			return false;
+		if (Double.doubleToLongBits(timeStamp) != Double
+				.doubleToLongBits(other.timeStamp))
+			return false;
+		return true;
+	}
+
 	public double getTimeStamp() {
 		return timeStamp;
 	}
